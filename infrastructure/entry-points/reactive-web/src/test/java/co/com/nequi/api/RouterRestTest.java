18b4aabfe2f0e86c8bce1730a505ea1568b470c9
Edgar Guerrero <eoguerre@nequi.com>
2024-10-17 11:57:00 -0500

    bug solved

--- infrastructure/entry-points/reactive-web/src/test/java/co/com/nequi/api/RouterRestTest.java
+++ infrastructure/entry-points/reactive-web/src/test/java/co/com/nequi/api/RouterRestTest.java
@@ -2,12 +2,15 @@ package co.com.nequi.api;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {RouterRest.class, ProductHandler.class})
 @WebFluxTest
 class RouterRestTest {
